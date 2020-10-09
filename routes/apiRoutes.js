f669e3670c2aaf802a55b24ad9d70d467ef3016f
Carlito Algarin <carlito1113@yahoo.com>
2020-10-09 16:21:25 -0400

    added a route to my apiroutes file in server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,16 @@
 const db = require("../db/db.json")
 const store = require("../db/store")
 
+
+module.exports = function (app) {
+app.get('/public', function (req, res) {
+    res.json(tableData);
+});
+}
+
+
+
+
 // GET route
 store.getNote()
 // POST route
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 
 // require("./routes/apiRoutes")(app);
 require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
 
 
 app.listen(PORT, () => console.log(`Listen on PORT: http://localhost:${PORT}`));
